09256f178b562684400c08d6d2e01d0e1ce0d450
carly <h.inayoshi.849@nitech.jp>
2021-08-16 13:26:52 +0900

    Add license notice in source files

--- app/src/main/java/com/inayoshi/atatechniques/directassignments/BitmapCache.java
+++ app/src/main/java/com/inayoshi/atatechniques/directassignments/BitmapCache.java
@@ -1,3 +1,21 @@
+/*
+    Test suite with anti-taint-analysis techniques.
+    Copyright (C) 2021  Nagoya Institute of Technology, Hiroki Inayoshi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package com.inayoshi.atatechniques.directassignments;
 
 import android.graphics.Bitmap;
